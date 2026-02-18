940fa6973459347234c5baa32c147077e163853a
YeldosR <rustembeky@gmail.com>
2026-02-18 23:50:14 +0500

    Add Practice2 Python basics examples

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,14 @@
+print(True)
+print(False)
+
+print(10 > 5)
+print(3 == 7)
+
+a = 10
+b = 20
+print(a < b)
+
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("Python"))
---
+++ Practice2/if_else/if_statement.py
@@ -0,0 +1,8 @@
+age = 18
+
+if age >= 18:
+    print("Adult")
+
+number = 5
+if number > 0:
+    print("Positive")
