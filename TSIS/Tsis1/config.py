6ad3e5215b45132e68b0a8718b75b518367c6968
YeldosR <rustembeky@gmail.com>
2026-04-30 09:34:06 +0500

    Add TSIS

---
+++ TSIS/Tsis1/config.py
@@ -0,0 +1,8 @@
+# config.py
+DB_CONFIG = {
+    "dbname": "phonebook_db", # Имя вашей базы данных
+    "user": "postgres",       # Имя пользователя БД
+    "password": "Reldos02", # ВАШ ПАРОЛЬ
+    "host": "localhost",
+    "port": "5432"
+}
